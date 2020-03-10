937c090f362fca0b6465e929a01ac61930cffb8f
Kombatross <konradnater1@wp.pl>
2020-03-10 11:55:33 +0100

    podstawyjs

---
+++ cz1/js/js/task0a.js
@@ -0,0 +1,34 @@
+// task0a
+
+var numberOfUsers = 3;
+var result = numberOfUsers / 2;
+alert(Math.floor(result));
+
+// task0b
+
+var pierwiastek = 144;
+alert(Math.sqrt(pierwiastek));
+
+// task01
+
+// var liczba
+
+alert(Math.floor(Math.random() * (100 - 5 + 1) + 5))
+
+// task02
+
+const widthBox = "23px";
+const widthPanel = "2px";
+var x = parseInt(widthBox);
+var y = parseInt(widthPanel);
+var result = x + y;
+alert(result);
+
+// task03
+
+var email = "cukierieczek@gmail.com";
+const check = email.indexOf("@");
+alert("znajduje się w " + check + " indeksie");
+
+
+
